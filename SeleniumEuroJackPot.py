ef31dce6fb9870d980a6c14292b325dd530442f2
Martin Break <marcin.przerwa1404@gmail.com>
2021-08-23 21:11:51 +0200

    .

--- SeleniumEuroJackPot.py
+++ SeleniumEuroJackPot.py
@@ -1,41 +1,42 @@
 from selenium import webdriver
-import numpy as np
 import time
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#test czy działa push
 
 driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
 driver.get('https://www.lotto.pl/eurojackpot/wyniki-i-wygrane')
+print("Strona została uruchomiona.")
 
 driver.implicitly_wait(10)
 skip1 = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div[4]/div/div/div[3]/div[2]/div[3]/button')
 skip1.click()
 skip2 = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div[4]/div/div/div[1]/div/div/div/div/button/i')
 skip2.click()
+print("Okienka zostały wyłączone.")
 
 # driver.implicitly_wait(30)
 # accept3 = driver.find_element_by_xpath('//*[@id="onesignal-slidedown-cancel-button"]')
 # accept3.click()
 
-# for more_sites in range(3):
-#     time.sleep(2)
-#     more_sites = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div[4]/div/div/div[2]/div[3]/div/button/span')
-#     more_sites.click()
+for scroll_site in range(20):
+    time.sleep(2)
+    scroll_site = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div[4]/div/div/div[2]/div[3]/div/button/span')
+    scroll_site.click()
+print("strona została przewinięta.")
 
 numerylos = []
-for i in driver.find_elements_by_class_name('result-item__number')[:10]:
+for i in driver.find_elements_by_class_name('result-item__number')[:420]:
     numerylos.append(i.text)
     numerylos[:] = [x for x in numerylos if x]  # usuwa puste elementy z listy
 
 numerywin = []
-for i in driver.find_elements_by_class_name('result-item__balls-box')[:5]:
+for i in driver.find_elements_by_class_name('result-item__balls-box')[:200]:
     numerywin.append(i.text.split('\n'))
 
 data = []
-for i in driver.find_elements_by_class_name('sg__desc-title')[:5]:
+for i in driver.find_elements_by_class_name('sg__desc-title')[:200]:
     data.append(i.text.strip(', godz. 20:00').strip('Pt., '))
-
 
 #zamiana list na numpy array
 numerylos = np.array(numerylos)
@@ -58,7 +59,8 @@ dict_win = dict(sorted(dict_win.items(), key=lambda item: item[1]))
 keys = dict_win.keys()
 values = dict_win.values()
 
-#pd.DataFrame(dict_los_win).to_csv("file.csv")
+pd.DataFrame(dict_los_win).to_csv("win_numbers.csv", header=["Nr", "Numer losowania", "Wylosowane 7 liczb","Data losowania","","","","",""])
+print("Liczby zostały zapisane w pliku win_numbers.csv")
 
 plt.figure(figsize=(20, 10))
 plt.xticks(fontsize = 10)
@@ -68,3 +70,4 @@ plt.bar(keys, values, edgecolor='black')
 for i, v in enumerate(values):
     plt.text(i, v+0.5, str(v), fontweight='bold')
 plt.savefig('chart.png')
+print("Wykres został utworzony.")
